eebf3df80860ab77e743720f948f800c9fa9a689
WindzoR <windzr67@gmail.com>
2021-07-28 13:43:45 +0300

    Прерывание блокированной нити. [#267413]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -2,7 +2,7 @@ package ru.job4j.concurrent;
 
 public class Wget {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Thread thread = new Thread(
                 () -> {
                     try {
@@ -12,10 +12,13 @@ public class Wget {
                             Thread.sleep(100);
                         }
                     } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
                         e.printStackTrace();
                     }
                 }
         );
         thread.start();
+        Thread.sleep(1000);
+        thread.interrupt();
     }
 }
