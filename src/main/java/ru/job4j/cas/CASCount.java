a751a68f655ea342f302ae5b5074ada3deb4409e
WindzoR <windzr67@gmail.com>
2021-09-09 10:34:21 +0300

    CAS - операции. fix 1.

--- src/main/java/ru/job4j/cas/CASCount.java
+++ src/main/java/ru/job4j/cas/CASCount.java
@@ -9,7 +9,13 @@ public class CASCount {
     private final AtomicReference<Integer> count = new AtomicReference<>(0);
 
     public void increment() {
-        count.getAndUpdate(v -> v + 1);
+        //count.getAndUpdate(v -> v + 1);
+        int temp;
+        int value;
+        do {
+            value = count.get();
+            temp = value + 1;
+        } while (!count.compareAndSet(value, temp));
     }
 
     public int get() {
